e66d459548746b77f26f0796c96a1d23ceae30aa
Dan Clawson <clawson@uen.org>
2015-04-04 07:50:07 -0600

    Add project files.

---
+++ Recipes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recipes
+//
+//  Created by Dan Clawson on 4/3/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
